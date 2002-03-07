e7038122fbeeb522b59a61a6b1f73ce18e8e2b3e
brian d foy <brian.d.foy@gmail.com>
2002-03-07 20:35:30 +0000

    * added support to change values with set() or unset().
    * you can parse multiple files with new_multiple.  i don't
    like the name though.
    
    * lots of niggling formatting fixes.  no big whoop.
    
    
    git-svn-id: http://brian-d-foy.svn.sourceforge.net/svnroot/brian-d-foy/trunk@102 d9f60a02-fc28-0410-9752-bc0d6c4f57a6

--- lib/ConfigReader/Simple.pm
+++ lib/ConfigReader/Simple.pm
@@ -63,18 +63,69 @@ sub new
 	my $filename = shift;
 	my $keyref   = shift;
 	
-	my $self  = {};
+	$keyref = [] unless defined $keyref;
 	
-	$self->{"filename"}  = $filename;
-	$self->{"validkeys"} = $keyref;
-	
-	bless $self, $class;
-	
-	$self->parse();
-	
+	my $self = $class->new_multiple( 
+		Files => [ $filename ],
+		Keys  => $keyref );
+			
 	return $self;
 	}
 
+=item new_multiple( Files => ARRAY_REF, Keys => ARRAY_REF )
+
+Create a configuration object from several files listed
+in the anonymous array value for the C<Files> key.  The
+module reads the files in the same order that they appear
+in the array.  Later values override earlier ones.  This
+allows you to specify global configurations which you 
+may override with more specific ones:
+
+	ConfigReader::Simple->new_multiple(
+		Files => [ qw( /etc/config /usr/local/etc/config /home/usr/config ) ],
+		);
+
+This function carps if the values are not array references.
+
+=cut
+	
+sub new_multiple
+	{
+	my $class    = shift;
+	my %args     = @_;
+
+	my $self = {};
+	
+	$args{'Keys'} = [] unless defined $args{'Keys'};
+	
+	carp( __PACKAGE__ . ': Files argument must be an array reference')
+		unless ref $args{'Files'} eq 'ARRAY';
+	carp( __PACKAGE__ . ': Keys argument must be an array reference')
+		unless ref $args{'Keys'} eq 'ARRAY';
+		
+	$self->{"filenames"} = $args{'Files'};
+	$self->{"validkeys"} = $args{'Keys'};
+	
+	bless $self, $class;
+	
+	foreach my $file ( @{ $self->{"filenames"} } )
+		{
+		$self->parse( $file );
+		}
+		
+	return $self;
+	}
+	
+sub new_from_prototype
+	{
+	my $self     = shift;
+	my $filename = shift;
+	
+	my $clone = $self->clone;
+	
+	return $clone;
+	}
+	
 sub AUTOLOAD
 	{
 	my $self = shift;
@@ -91,7 +142,7 @@ sub DESTROY
 	return 1;
 	}
 
-=item parse ()
+=item parse( FILENAME )
 
 This does the actual work.  No parameters needed.
 
@@ -100,30 +151,30 @@ the configuration file by calling C<parse()> again.
 
 =cut
 
-sub parse {
-   my $self = shift;
-
-   open(CONFIG, $self->{"filename"}) || 
-      die "Config: Can't open config file " . $self->{"filename"} . ": $!";
-
-   while (<CONFIG>) {
-      chomp;
-
-      next if /^\s*$/;  # blank
-      next if /^\s*#/;  # comment
-
-      my ($key, $value) = &parse_line($_);
-      warn "Key:  '$key'   Value:  '$value'\n" if $DEBUG;
-      
-      $self->{"config_data"}{$key} = $value;
-   }
-   close(CONFIG);
-   
- 	$self->_validate_keys;
-
-   return 1;
-
-}
+sub parse 
+	{
+	my $self = shift;
+	my $file = shift;
+	
+	open CONFIG, $file or die "Cannot open file $file: $!";
+	
+	while( <CONFIG> )
+		{
+		chomp;
+		next if /^\s*(#|$)/; 
+		
+		my ($key, $value) = &parse_line($_);
+		warn "Key:  '$key'   Value:  '$value'\n" if $DEBUG;
+		
+		$self->{"config_data"}{$key} = $value;
+		}
+		
+	close(CONFIG);
+	
+	$self->_validate_keys;
+	
+	return 1;
+	}
 
 =item get( DIRECTIVE )
 
@@ -134,9 +185,65 @@ Returns the parsed value for that directive.
 sub get 
 	{
 	my $self = shift;
-	my $key = shift;
+	my $key  = shift;
 	
 	return $self->{"config_data"}{$key};
+	}
+
+=item set( DIRECTIVE, VALUE )
+
+Sets the value for DIRECTIVE to VALUE.  The DIRECTIVE
+need not already exist.  This overwrites previous 
+values.
+
+=cut
+
+sub set 
+	{
+	my $self = shift;
+	my( $key, $value ) = @_;
+	
+	$self->{"config_data"}{$key} = $value;
+	}
+
+=item unset( DIRECTIVE )
+
+Remove the value from DIRECTIVE, which will still exist.  It's
+value is undef.  If the DIRECTIVE does not exist, it will not
+be created.  Returns FALSE if the DIRECTIVE does not already
+exist, and TRUE otherwise.
+
+=cut
+
+sub unset
+	{
+	my $self = shift;
+	my $key  = shift;
+	
+	return unless $self->exists( $key );
+	
+	$self->{"config_data"}{$key} = undef;
+	
+	return 1;
+	}
+
+=item remove( DIRECTIVE )
+
+Remove the DIRECTIVE. Returns TRUE is DIRECTIVE existed
+and FALSE otherwise.   
+
+=cut
+
+sub remove
+	{
+	my $self = shift;
+	my $key  = shift;
+	
+	return unless $self->exists( $key );
+	
+	delete $self->{"config_data"}{$key};
+	
+	return 1;
 	}
 
 =item directives()
@@ -157,6 +264,9 @@ sub directives
 
 =item exists( DIRECTIVE )
 
+Return TRUE if the specified directive exists, and FALSE
+otherwise.  
+
 =cut
 
 sub exists
@@ -166,24 +276,55 @@ sub exists
 	
 	return CORE::exists $self->{"config_data"}{ $name };
 	}
+
+=item clone
+
+Return a copy of the object.  The new object is distinct
+from the original.
+
+=cut
+
+# this is only the first stab at this -- from 35,000
+# feet in coach class
+sub clone
+	{
+	my $self = shift;
 	
+	my $clone = {};
+	
+	$clone->{"filename"}  = $self->{"filename"};
+	$clone->{"validkeys"} = $self->{"validkeys"};
+	
+	foreach my $key ( keys %{ $self->{'config_data'} } )
+		{
+		$clone->{'config_data'}{$key} = $self->{'config_data'}{$key};
+		}
+			
+	bless $clone, __PACKAGE__;
+	
+	return $clone;
+	}
+
 # Internal methods
 
-sub parse_line {
-   my $text = shift;
-
-   my ($key, $value);
-
+sub parse_line 
+	{
+	my $text = shift;
+	
+	my ($key, $value);
+	
 	# AWJ: Allow optional '=' or ' = ' between key and value:
-   if ($text =~ /^\s*(\w+)\s*[=]?\s*(['"]?)(.*?)\2\s*$/ ) {
-      $key = $1;
-      $value = $3;
-   } else {
-      croak "Config: Can't parse line: $text\n";
-   }
-
-   return ($key, $value);
-}
+	if ($text =~ /^\s*(\w+)\s*[=]?\s*(['"]?)(.*?)\2\s*$/ ) 
+		{
+		( $key, $value ) = ( $1, $3 );
+		} 
+	else 
+		{
+		croak "Config: Can't parse line: $text\n";
+		}
+	
+	return ($key, $value);
+	}
 
 
 =item _validate_keys ( )
