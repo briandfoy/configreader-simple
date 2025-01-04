4a67971af676993aba0fd0ae13b1011c780dcb97
brian d foy <briandfoy@pobox.com>
2025-01-04 11:04:37 -0500

    bump version

--- lib/ConfigReader/Simple.pm
+++ lib/ConfigReader/Simple.pm
@@ -9,7 +9,7 @@ use vars qw($VERSION $AUTOLOAD %ERROR $ERROR $Warn $Die);
 use Carp qw(croak carp);
 
 $ERROR     = '';
-$VERSION   = '1.296';
+$VERSION   = '1.297';
 $Warn      = 0;
 $Die       = '';
 
